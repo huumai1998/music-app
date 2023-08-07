fa1b7132239f5431385993dd108b2dcaf26d97b4
huumai1998 <huumai0918@gmail.com>
2023-08-07 10:52:42 -0600

    idk

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,10 @@
 
 import {usePathname} from "next/navigation";
 import { useMemo } from "react";
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -14,11 +18,13 @@ const Sidebar: React.FC<SidebarProps> = ({
 
     const routes = useMemo(() => [
         {
-            label: "Home",
+            icon: HiHome,
+            label: 'Home',
             active: pathname !== "/search",
             href: '/',
         },
         {
+            icon: BiSearch,
             label: 'Search',
             active: pathname !== '/search',
             href: '/search'
@@ -26,8 +32,39 @@ const Sidebar: React.FC<SidebarProps> = ({
     ], [pathname])
 
     return (
-        <div>
-            {children}
+        <div className="flex h-full">
+            <div 
+                className="
+                hidden 
+                md:flex 
+                flex-col 
+                gap-y-2 
+                bg-black 
+                h-full 
+                w-[300px] 
+                p-2">
+                <Box>
+                    <div className="
+                        flex
+                        flex-col
+                        gap-y-4
+                        px-5
+                        py-4
+                        "
+                        >
+                            {routes.map((item) => (
+                                <SidebarItem 
+                                    key={item.label}
+                                    {...item}
+                                />
+                            ))}
+                    </div>
+                </Box>
+                <Box
+                    className="overflow-y-auto h-full">
+                    Song Libary
+                </Box>
+            </div>
         </div>
     )
 }
