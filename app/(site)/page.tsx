3f07143ec0658f424f53bbaa777b77b1bb2f1736
huumai1998 <huumai0918@gmail.com>
2023-08-09 22:55:40 -0600

    addOnClickButton

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -23,6 +23,13 @@ export default function Home() {
                 </div>
               </div>
             </Header>
+            <div className="mt-2 mb-7 px-6">
+              <div className="flex justify-between items-center">
+                <h1 className="text-white text-2xl font-semibold">
+                  Newest songs
+                </h1>
+              </div>
+            </div>
     </div>
     </>
   )
