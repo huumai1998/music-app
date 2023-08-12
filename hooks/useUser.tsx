886e8f2334ed73d74e1ebfe55f8f8b3c28c22328
huumai1998 <huumai0918@gmail.com>
2023-08-12 04:33:23 -0600

    addValue

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,7 +1,7 @@
 import { Subscription, UserDetails } from "@/types/types";
 import { User } from "@supabase/auth-helpers-nextjs";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type UserContextType = {
     accessToken: string | null;
@@ -66,4 +66,25 @@ export const MyUserContextProvider = (props: Props) => {
             setSubscription(null);
           }
     }, [user, isLoadingUser]);
+
+    const value = {
+        accessToken,
+        user,
+        userDetails,
+        isLoading: isLoadingUser || isLoadingData,
+        subscription
+      };
+    
+      return <UserContext.Provider value={value} {...props} />;
+    };
+    
+    export const useUser = () => {
+      const context = useContext(UserContext);
+      if (context === undefined) {
+        throw new Error(`useUser must be used within a MyUserContextProvider.`);
+      }
+      return context;
+    };
 }
+
+
