ff75a01d2b13ad47e85ecf4163121589cb4d912d
huumai1998 <huumai0918@gmail.com>
2023-08-18 20:34:28 -0600

    getSongByID

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,12 +2,13 @@ import getSongs from "@/action/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import PageContent from "./components/PageContent";
+import getSongsByUserId from "@/action/getSongByUserId";
 
 export const revalidate = 0;
 
 export default async function Home() {
 
-  const songs = await getSongs();
+  const songs = await getSongsByUserId();
   return (
     <>
     <div
