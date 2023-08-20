bb5399a7573bc922c16ba0c9de0f7f863c7c7dbb
huumai1998 <huumai0918@gmail.com>
2023-08-19 20:22:34 -0600

    optionallsongdata

--- action/getSongsOptional.ts
+++ action/getSongsOptional.ts
@@ -1,24 +1,18 @@
+// get all songs data
+
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 import { Song } from "@/types/types";
 
-const getSongs = async (): Promise<Song[]> => {
+const getSongsOptional = async (): Promise<Song[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies
   });
 
-  const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
-
-  if (sessionError) {
-    console.log(sessionError.message);
-    return [];
-  }
-
   const { data, error } = await supabase
     .from('songs')
     .select('*')
-    .eq('user_id', sessionData.session?.user.id)
     .order('created_at', { ascending: false })
 
   if (error) {
@@ -28,4 +22,4 @@ const getSongs = async (): Promise<Song[]> => {
   return (data as any) || [];
 };
 
-export default getSongs;
+export default getSongsOptional;
